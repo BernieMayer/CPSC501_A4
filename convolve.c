3c6e6a44f635bd82ebcb65c8df7a323cb9fce626
bemayer <bemayer@ucalgary.ca>
2016-11-28 16:24:15 -0700

    Starting the work on the audio file header

---
+++ convolve.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct AudioFileHeader readHeaderOfAudioFile(FILE* file);
+
+struct  AudioFileHeader
+{
+  int chunkId;
+  int chunkSize;
+  int format;
+
+  int subchunk1ID;
+  int subchunk1Size;
+  unsigned short audioFormat;
+  unsigned short numChannels;
+  int sampleRate;
+  int byteRate;
+  unsigned short blockAlign;
+  unsigned short bitsPerSample;
+  int subchunk2ID;
+  int subchunk2Size;
+};
+
+
+struct AudioFileHeader readHeaderOfAudioFile(FILE* file)
+{
+  int j = 0;
+  int i = 0;
+  struct AudioFileHeader audioHeader;
+  fscanf(file, "%d", &i);
+  while (!feof(file))
+  {
+    printf("%d\n", i);
+    if (j == 0)
+      audioHeader.chunkId = i;
+    else if (j == 1)
+      audioHeader.chunkSize = i;
+    else if (j == 2)
+      audioHeader.format = i;
+    else if (j == 3)
+      audioHeader.subchunk1ID = i;
+    else if (j == 4)
+      audioHeader.subchunk1Size = i;
+    else if (j == 5)
+    {
+      audioHeader.audioFormat = i & 0xF;
+      audioHeader.numChannels = (i >> 4) & 0xF;
+    } else if ( j == 6)
+      audioHeader.sampleRate = i;
+    else if (j == 7)
+      audioHeader.byteRate = i;
+    else if (j == 8)
+    {
+      audioHeader.blockAlign = i & 0xF;
+      audioHeader.bitsPerSample = (i >> 4) & 0xF;
+    } else if ( j == 9)
+      audioHeader.subchunk2ID = i;
+    else if (j == 10)
+      audioHeader.subchunk2Size = i;
+    fscanf(file, "%d", &i);
+    j++;
+  }
+  fclose(file);
+  return audioHeader;
+}
+
+int main(int argc, char * argv[])
+{
+  /*
+  Testing splitting an int
+  int num = 0b00110010;
+  unsigned short part1 = num & 0xF;
+  unsigned short part2 = (num >> 4) &0xF;
+  printf("The num is %d , part1 is %hu part2 is %hu", num, part1, part2);
+  */
+
+
+  FILE* audioFile;
+  if (argc > 1)
+  {
+      char* fileName = argv[1];
+      printf("The file name is %s \n" , fileName);
+      audioFile = fopen(fileName, "r");
+      struct AudioFileHeader audioHeader = readHeaderOfAudioFile(audioFile);
+  }
+
+
+  return 0;
+}
