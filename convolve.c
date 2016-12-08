a1529117c573ead73be647074f9b6da3fa39fc16
bemayer <bemayer@ucalgary.ca>
2016-12-07 19:49:55 -0700

    Added some fast fourier code

--- convolve.c
+++ convolve.c
@@ -107,6 +107,64 @@ struct AudioFileHeader readHeaderOfAudioFile(FILE* file)
   return audioHeader;
 }
 
+//  The four1 FFT from Numerical Recipes in C,
+//  p. 507 - 508.
+//  Note:  changed float data types to double.
+//  nn must be a power of 2, and use +1 for
+//  isign for an FFT, and -1 for the Inverse FFT.
+//  The data is complex, so the array size must be
+//  nn*2. This code assumes the array starts
+//  at index 1, not 0, so subtract 1 when
+//  calling the routine
+
+void four1(double data[], int nn, int isign)
+{
+    unsigned long n, mmax, m, j, istep, i;
+    double wtemp, wr, wpr, wpi, wi, theta;
+    double tempr, tempi;
+
+    n = nn << 1;
+    j = 1;
+
+    for (i = 1; i < n; i += 2) {
+	if (j > i) {
+	    SWAP(data[j], data[i]);
+	    SWAP(data[j+1], data[i+1]);
+	}
+	m = nn;
+	while (m >= 2 && j > m) {
+	    j -= m;
+	    m >>= 1;
+	}
+	j += m;
+    }
+
+    mmax = 2;
+    while (n > mmax) {
+	istep = mmax << 1;
+	theta = isign * (6.28318530717959 / mmax);
+	wtemp = sin(0.5 * theta);
+	wpr = -2.0 * wtemp * wtemp;
+	wpi = sin(theta);
+	wr = 1.0;
+	wi = 0.0;
+	for (m = 1; m < mmax; m += 2) {
+	    for (i = m; i <= n; i += istep) {
+		j = i + mmax;
+		tempr = wr * data[j] - wi * data[j+1];
+		tempi = wr * data[j+1] + wi * data[j];
+		data[j] = data[i] - tempr;
+		data[j+1] = data[i+1] - tempi;
+		data[i] += tempr;
+		data[i+1] += tempi;
+	    }
+	    wr = (wtemp = wr) * wpr - wi * wpi + wr;
+	    wi = wi * wpr + wtemp * wpi + wi;
+	}
+	mmax = istep;
+    }
+}
+
 
 //Convolve function using Fast Fourier Transform
 //(might want to convoert to doubles)
@@ -140,12 +198,10 @@ void convolveFFT(float x[], int N, float h[], int M, float y[], int P)
       padded_h[j] = 0.0f;
   }
 
-  //For each segment..
+  //Fill the segmentArray_x
   for (int i = 0; i < numSegmentsX; i++)
   {
-    //Apply fast Fourier Transform
-
-
+    
   }
 
 
